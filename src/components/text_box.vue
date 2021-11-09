5276c12aa409b6f64ea7d078c7f35a581dd49619
Boghos Youseef <43525406+BoghosYouseef@users.noreply.github.com>
2021-11-09 20:11:11 +0100

    Source files for test-project-v2
    uploading files to the repository for backup

---
+++ src/components/text_box.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="textBoxes">
+        <input id="userName" type="text">
+        <input id="password" type="password">
+    <div>
+</template>
+
+<script>
+export default {
+    textBoxes,
+}
+</script>
+
+<script>
+
+</script>
---
+++ tests/e2e/specs/MainPageNav.feature
@@ -0,0 +1,7 @@
+Feature: Navigate to Main Page
+
+    I want to open a user registration website
+    
+    Scenario: Opening the registeration page
+    Given I open registration page
+    Then I see "User Registration" in the title
